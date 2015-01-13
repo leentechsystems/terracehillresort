0938bdac9f31f9774de9de3ee813e02d0b87aa8a
patric025 <patrivera25@gmail.com>
2015-01-13 11:21:49 +0800

    modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -49,11 +49,11 @@ function onOnline() {
     $("#message").addClass('hide');
     $("#main_message").addClass('hide');
     $("#sub_message").addClass('hide');
-    window.location = "http://m.meiceljewelry.com/";
-     // setTimeout(function(){
-     //        var ref = window.open('http://www.wufoo.com/html5/attributes/07-accept.html', '_blank', 'location=no');
-     //        ref.addEventListener('loaderror', function(event) { ref.close(); location.reload(); });
-     // }, 3000);
+    //window.location = "http://m.meiceljewelry.com/";
+     setTimeout(function(){
+            var ref = window.open('http://m.meiceljewelry.com/', '_blank', 'location=no');
+            ref.addEventListener('loaderror', function(event) { ref.close(); location.reload(); });
+     }, 3000);
 }
 
 
